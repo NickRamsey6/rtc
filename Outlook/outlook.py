fc9a343196b02f5f7b07ebc0ee8a315977f6a0ed
Nick Ramsey <nramseysc@gmail.com>
2024-04-12 23:50:46 -0600

    add notes

--- Outlook/outlook.py
+++ Outlook/outlook.py
@@ -1,10 +1,14 @@
 from O365 import Account
 
+# Access Account
 credentials = ('client_id', 'client_secret')
-
 account = Account(credentials)
+
+# Send Email
 m = account.new_message()
 m.to_add('to_email@example.com')
 m.subject = 'Test!'
 m.body = "This is a test message"
 m.send()
+
+# Retrieve Email
