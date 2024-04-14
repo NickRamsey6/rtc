5df646bd8691be9e076b747d9d2a9993f30eb104
Nick Ramsey <nramseysc@gmail.com>
2024-04-13 23:34:04 -0600

    add notes on dupes

--- Excel/excel.py
+++ Excel/excel.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
+# Create new csv with only the duplicated rows
 def get_dupes_df(dataframe):
     df = dataframe
     duplicates_df = df[df.duplicated()]
+
+    # Can also pass in specific cols to check for dupes
+    # duplicates_df = df[df/duplicated(cols)]
 
     duplicates_df.to_csv('dupes.csv')
